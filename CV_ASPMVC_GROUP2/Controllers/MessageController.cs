8a5ad90cb5b21567dfc8f0bf09368a2b40a23fa7
filippahelin <121015432+filippahelin@users.noreply.github.com>
2023-12-27 13:32:34 +0100

    meddelande-funktioner

--- CV_ASPMVC_GROUP2/Controllers/MessageController.cs
+++ CV_ASPMVC_GROUP2/Controllers/MessageController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using CV_ASPMVC_GROUP2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+using System.Security.Claims;
 
 namespace CV_ASPMVC_GROUP2.Controllers
 {
     public class MessageController : Controller
     {
-        public IActionResult Index()
+
+        private readonly TestDbContext _context;
+
+        public MessageController(TestDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult SendMessage(string receiverId)
+        {
+            var message = new Message
+            {
+                ToUserId = receiverId 
+            };
+
+            return View("SendMessage");
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message m)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Messages.Add(m);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+
+            }
+
+            return View("SendMessage"); 
+        }
+
+
+        public IActionResult Inbox()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var incomingMessages = _context.Messages.Where(m => m.ToUserId == userId).ToList();
+
+            return View(incomingMessages);
         }
     }
 }
