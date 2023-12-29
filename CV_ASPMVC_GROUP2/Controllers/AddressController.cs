7a93822cc8a9f614b5d409e49dea8b30d72f34ef
Theo <theo.sohlman@gmail.com>
2023-12-29 15:41:59 +0100

    addresscontroller och EditAddress funktion

---
+++ CV_ASPMVC_GROUP2/Controllers/AddressController.cs
@@ -0,0 +1,51 @@
+﻿using CV_ASPMVC_GROUP2.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CV_ASPMVC_GROUP2.Controllers
+{
+    [Authorize]
+    public class AddressController : BaseController
+    {
+        private readonly TestDbContext _context;
+        public AddressController(TestDbContext testDbContext)
+        {
+            _context = testDbContext;
+        }
+        public IActionResult EditAddress()
+        {
+            var currentUserId = base.UserId;
+            var address = _context.Addresses.Where(a => a.UserId.Equals(currentUserId)).Single();
+            var model = new EditAddressViewModel
+            {
+                Street = address.Street,
+                City = address.City,
+                PostalCode = address.PostalCode.ToString()
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditAddress(EditAddressViewModel editAddressViewModel)
+        {
+            try
+            {
+                var currentUserId = base.UserId;
+                var address = _context.Addresses.Where(a => a.UserId.Equals(currentUserId)).Single();
+
+                address.Street = editAddressViewModel.Street;
+                address.City = editAddressViewModel.City;
+                address.PostalCode = Int32.Parse(editAddressViewModel.PostalCode);
+                
+                var result = await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex) 
+            { 
+                return View(editAddressViewModel);
+            }
+            
+        }
+    }
+}
---
+++ CV_ASPMVC_GROUP2/Models/EditAddressViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CV_ASPMVC_GROUP2.Models
+{
+    public class EditAddressViewModel
+    {
+        [Required(ErrorMessage = "Vänligen skriv din gatu-adress och gatu-nummer.")]
+        [Display(Name = "Gatu-adress & husnummer")]
+        public string Street { get; set; }
+        [Required(ErrorMessage = "Vänligen ange stad.")]
+        [Display(Name = "Stad")]
+        public string City { get; set; }
+        [RegularExpression("^\\d+$", ErrorMessage ="Postkoden måste bestå av Siffror.")]
+        [Display(Name = "Postkod")]
+        [StringLength(5)]
+        public string PostalCode { get; set; }
+    }
+}
