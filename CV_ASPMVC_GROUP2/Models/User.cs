f973ab761c9f69ccf4c3b9abc5914d29d2cb05d1
FilippaEm <filippa.emberg@hotmail.se>
2023-12-29 10:35:43 +0100

    Lägga till bild till ett nytt projekt

--- CV_ASPMVC_GROUP2/Models/User.cs
+++ CV_ASPMVC_GROUP2/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CV_ASPMVC_GROUP2.Models
 {
@@ -9,6 +10,9 @@ namespace CV_ASPMVC_GROUP2.Models
         public string? ProfilePicture {  get; set; }
         public virtual Cv? Cv { get; set; }
         public virtual Address? Address { get; set; }
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
         public virtual IEnumerable<Message> SentMessages { get; set; } = new List<Message>();
         public virtual IEnumerable<Message> RecievedMessages { get; set; } = new List<Message>();
         public virtual IEnumerable<UserProject> UserProjects { get; set; } = new List<UserProject>();
